9e6dfe519c711c295aa72dbfd88afc8571d8e29e
Anne Heijink <anne@heijink.org>
2024-03-20 11:35:06 +0100

    add js

---
+++ scripts/fullscreen.mjs
@@ -0,0 +1,14 @@
+Hooks.once('ready', async function () {
+	let settingsDiv = document.getElementById("settings-access");
+	settingsDiv.insertAdjacentHTML('afterend', '<div id="foundry-mobile-module-toggle-fullscreen"><button data-action="fullscreen" id="foundry-mobile-module-btn-fullscreen"><i class="fas fa-maximize"></i>Toggle fullscreen</button></div>')
+	let btn = document.getElementById("foundry-mobile-module-btn-fullscreen");
+	btn.addEventListener('click', toggleFullScreen);
+});
+
+function toggleFullScreen() {
+	if (!document.fullscreenElement) {
+		document.documentElement.requestFullscreen();
+	} else if (document.exitFullscreen) {
+		document.exitFullscreen();
+	}
+}
