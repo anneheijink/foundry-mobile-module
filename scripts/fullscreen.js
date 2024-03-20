97172b271e5541a5a3fd1815fb1203dc1463dbfe
Anne Heijink <anne@heijink.org>
2024-03-20 11:41:53 +0100

    add console log

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -1,4 +1,6 @@
+console.log("loaded fullscreen script");
 Hooks.once('ready', async function () {
+	console.log("adding fullscreen button...");
 	let settingsDiv = document.getElementById("settings-access");
 	settingsDiv.insertAdjacentHTML('afterend', '<div id="foundry-mobile-module-toggle-fullscreen"><button data-action="fullscreen" id="foundry-mobile-module-btn-fullscreen"><i class="fas fa-maximize"></i>Toggle fullscreen</button></div>')
 	let btn = document.getElementById("foundry-mobile-module-btn-fullscreen");
